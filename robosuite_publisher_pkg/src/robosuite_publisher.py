84915bcc608f6138a1b1d0ae8faada363fbaf806
Tobias <fuz.sh0710@gmail.com>
2024-08-27 10:55:32 -0400

    modfiying files

--- robosuite_publisher_pkg/src/robosuite_publisher.py
+++ robosuite_publisher_pkg/src/robosuite_publisher.py
@@ -12,7 +12,8 @@ controller_config = load_controller_config(default_controller="OSC_POSE")
 
 # create environment instance
 env = suite.make(
-    env_name="Door", # try with other tasks like "Stack" and "Door"
+    env_name="Lift", # try with other tasks like "Stack" and "Door"
+    reward_shaping=True,
     robots="Kinova3",  # try with other robots like "Sawyer" and "Jaco"
     has_renderer=True,
     controller_configs=controller_config,
@@ -30,27 +31,96 @@ env = suite.make(
 #     if done:
 #         obs = wrapped_env.reset()
 
+def euler_to_quaternion(roll, pitch, yaw):
+    """
+    Convert Euler angles to quaternion.
+
+    Args:
+    - roll: Rotation around x-axis in radians (float)
+    - pitch: Rotation around y-axis in radians (float)
+    - yaw: Rotation around z-axis in radians (float)
+
+    Returns:
+    - Quaternion [w, x, y, z] (list of floats)
+    """
+
+    cy = np.cos(yaw * 0.5)
+    sy = np.sin(yaw * 0.5)
+    cp = np.cos(pitch * 0.5)
+    sp = np.sin(pitch * 0.5)
+    cr = np.cos(roll * 0.5)
+    sr = np.sin(roll * 0.5)
+
+    qw = cr * cp * cy + sr * sp * sy
+    qx = sr * cp * cy - cr * sp * sy
+    qy = cr * sp * cy + sr * cp * sy
+    qz = cr * cp * sy - sr * sp * cy
+
+    return [qw, qx, qy, qz]
+
+robot_obs = [0] * 40
+robot_obs = np.array(robot_obs)
+
+def obs_callback(msg):
+    global robot_obs
+    robot_obs = np.array(msg.data)
+
+def format_obs(modded_observations):
+
+    formatted_obs = np.array([])
+    count = 0
+    for key in modded_observations:
+        if count < 7:
+            print(key)
+            formatted_obs = np.concatenate((formatted_obs, modded_observations[key]))
+        count += 1
+    formatted_obs = np.concatenate((modded_observations["object-state"], formatted_obs))
+
+    return formatted_obs
+
 def array_publisher(wrapped_env, model):
     rospy.init_node('array_publisher', anonymous=True)
     pub = rospy.Publisher('array_topic', Array, queue_size=10)
+    rospy.Subscriber('/my_gen3/robot_deets_topic', Array, obs_callback)
+    # rospy.Subscriber('array_topic', Array, array_callback)
+
+    # pub2 = rospy.Publisher('start_topic', Array)
+    # print("yes")
+    rospy.loginfo("YES")
 
     running = True
     count = 0
     obs = wrapped_env.reset()
 
-    rate = rospy.Rate(10)  # 10Hz
+    rate = rospy.Rate(1)  # 1Hz
+    obs_unwrapped = wrapped_env.unwrapped
+    obs_unwrapped = obs_unwrapped.reset()
+    
     while not rospy.is_shutdown() and running:
+    # while not rospy.is_shutdown():
+    # while running:
         # Example array data (floats)
+        # robot_deets = rospy.wait_for_message("robot_deets_topic", Array)
         array_data = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
 
-        action, _states = model.predict(obs, deterministic=True)
+        obs_unwrapped = GymWrapper.observation_spec(wrapped_env)
+        print(len(robot_obs))
+
+        # getting these values made up soon for object state
+
+        robot_obs_half = np.concatenate((obs_unwrapped["object-state"], robot_obs))
+
+        # action, _states = model.predict(obs, deterministic=True)
+        action, _states = model.predict(robot_obs_half, deterministic=True)
         obs, reward, done, info = wrapped_env.step(action)
 
         array_data = np.array(action)
         if done:
             obs = wrapped_env.reset()
+        
         count += 1
-        if count == 50:
+        rospy.loginfo(count)
+        if count == 50000:
             running = False
 
         # Create a message object
@@ -59,16 +129,20 @@ def array_publisher(wrapped_env, model):
 
         # Publish the message
         pub.publish(msg)
-
-        rate.sleep()
+        
+        rospy.sleep(1.0)
+        # rate.sleep()
 
 if __name__ == '__main__':
     wrapped_env = GymWrapper(env)
 
     model = SAC("MlpPolicy", wrapped_env, verbose=1)
-    model.learn(total_timesteps=100, log_interval=4)
+    model.learn(total_timesteps=500, log_interval=4)
 
     try:
+        # rospy.loginfo("GO")
         array_publisher(wrapped_env, model)
+        # print("WHAT")
     except rospy.ROSInterruptException:
         pass
+    # print("DONE")
--- test_subscriber/src/subscriber.py
+++ test_subscriber/src/subscriber.py
@@ -3,16 +3,32 @@
 import rospy
 from robosuite_publisher_pkg.msg import Array  # Import custom message type
 
+# from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
+
+movement_arr = None
+
 def array_callback(msg):
-    rospy.loginfo("Received array: %s", msg.data)
+    global movement_arr
+    movement_arr = msg
+    # rospy.loginfo("Received array: %s", msg.data)
+    rospy.loginfo("Received array: %s", movement_arr)
 
 def array_subscriber():
-    rospy.init_node('array_subscriber', anonymous=True)
-    rospy.Subscriber('array_topic', Array, array_callback)
+    # rospy.init_node('array_subscriber', anonymous=True)
+    # rospy.Subscriber('array_topic', Array, array_callback)
+    # rospy.spin()
+    rospy.init_node('pose_subscriber', anonymous=True)
+    # rospy.Subscriber('pose_topic', PoseStamped, array_callback)
+    # rospy.Subscriber('/my_gen3/robot_deets_topic', Array, array_callback)
     rospy.spin()
 
 if __name__ == '__main__':
+    
+    # print(robot_deets)
     try:
+        robot_deets = rospy.wait_for_message('/my_gen3/robot_deets_topic', Array)
+        print(robot_deets)
         array_subscriber()
     except rospy.ROSInterruptException:
         pass
